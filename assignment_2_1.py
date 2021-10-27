b7013a9d747783afe5995c2d221e0adf8a369010
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-10-27 09:09:09 +0300

    1

---
+++ assignment_2_1.py
@@ -0,0 +1,20 @@
+# assignment statements that perform the following operations with variables a,b and c
+#add 2 to a and asssign the result to b
+
+def main():
+    a = 1
+    print("a=:", a)
+
+    b = 2 + a
+    print("b=:", b)
+
+    a = b*4
+    print("a=:", a)
+
+    b = a/3.14
+    print("b=:", b)
+
+    a = b-8
+    print("a=:", a)
+
+main()
